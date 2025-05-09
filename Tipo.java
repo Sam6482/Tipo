a36633de76e646ec3fa8e6f7119cb4bc7ff1882f
Sam6482 <20243ds141@utez.edu.mx>
2025-05-09 13:43:32 -0600

    clasee de tipo

--- Tipo.java
+++ Tipo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Tipo{
     public static void main(String[] args){
-        Scanner teclado = new Scanner(System.in);
+        Scanner scanner= new Scanner(System.in);
 
         System.out.println("Escribe tu nombre");
         String nombre = scanner.nextLine();
@@ -11,12 +11,12 @@ public class Tipo{
         char inicial = scanner.next().charAt(0);
 
          System.out.println("Ingresa tu estatura en metros");
-         int edad = scanner.next().nextInt();
+         int edad = scanner.nextInt();
         
          System.out.println("¿Eres estudiante? (true/false)");
-         boolean esEstudiante = scanner.nextBoleean();
+         boolean esEstudiante = scanner.nextBoolean();
 
-
+        System.out.println("Tu nombre es"+nombre);
          
         
         
