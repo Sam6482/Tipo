8b30c6a2baea62cd419772ffd0e157c8b8f1a169
Sam6482 <20243ds141@utez.edu.mx>
2025-05-09 13:34:34 -0600

    Creacion de tipos de Datos

---
+++ Tipo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tipo{
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Escribe tu nombre");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Ingresa la inicial de tu nonmbre");
+        char inicial = scanner.next().charAt(0);
+
+         System.out.println("Ingresa tu estatura en metros");
+         int edad = scanner.next().nextInt();
+        
+         System.out.println("¿Eres estudiante? (true/false)");
+         boolean esEstudiante = scanner.nextBoleean();
+
+
+         
+        
+        
+    }
+}
